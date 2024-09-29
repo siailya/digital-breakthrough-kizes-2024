5c0e4fe7220378f6bf209453eda8eca49b5a7f21
Nogaromo <47574456+Nogaromo@users.noreply.github.com>
2024-09-29 08:33:58 +0300

    Add files via upload

--- whisper/request.py
+++ whisper/request.py
@@ -1,31 +1,11 @@
-import tritonclient.grpc as grpcclient
-import librosa
-import numpy as np
+import requests
+import os
 
-
-from tritonclient.utils import np_to_triton_dtype
-
-
-model_name = "whisper_hf"
-server_url = "localhost:8001"
-triton_client = grpcclient.InferenceServerClient(url=server_url)
-
-audio, sample_rate = librosa.load("/work/cut_output_mono.wav")
-audio = np.array(audio).reshape((1, -1))
-sample_rate = np.array(sample_rate).reshape((1, -1)).astype(np.int32)
-
-inputs = [
-        grpcclient.InferInput(
-            "audio", audio.shape, np_to_triton_dtype(audio.dtype)
-        ),
-        grpcclient.InferInput(
-            "sample_rate", sample_rate.shape, np_to_triton_dtype(sample_rate.dtype)
-        )
-    ]
-inputs[0].set_data_from_numpy(audio)
-inputs[1].set_data_from_numpy(sample_rate)
-
-result = triton_client.infer(model_name, inputs)
-print(result.as_numpy("timestamps"))
-for row in result.as_numpy("texts"):
-    print(row.decode("utf-8"))
+response = requests.post(
+            url='http://localhost:8000/get_transcripton',
+            # data = {'sample_rate': str(44100)},
+            files={'audio': open(os.path.join("audio_wav", "9IH9ghkWEvg.wav"), 'rb'),
+                   'type': 'audio/wav'},
+            headers={'accept': 'application/json'}
+                             )
+print(response)
